30ec677be16fe130861ca730e292219408475a0e
Christian Mancini <mancinichristian00@gmail.com>
2024-06-18 18:57:50 +0200

    Added resize function

--- Image.cpp
+++ Image.cpp
@@ -12,6 +12,11 @@
 #include "stb_image_write.h"
 #endif // STB_IMAGE_WRITE_IMPLEMENTATION
 
+#ifndef STB_IMAGE_RESIZE_IMPLEMENTATION
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb_image_resize2.h"
+#endif // STB_IMAGE_RESIZE_IMPLEMENTATION
+
 using namespace std;
 
 void Image::loadImage(const std::string &image_path, Image &image) {
@@ -30,6 +35,17 @@ void Image::saveImage(const std::string &output_path, Image image) {
     stbi_image_free(image.rgb_image);
 }
 
-void Image::resizeImage(int new_width, int new_height, Image &image) {
+Image Image::resizeImage(const Image &image, int new_width, int new_height) {
+    Image resized_image;
+    auto *resized_image_data = new unsigned char[new_width * new_height * 3];
+    stbir_resize_uint8_srgb(image.rgb_image, image.width, image.height, image.width * 3, resized_image_data, new_width, new_height, new_width * 3,
+                            static_cast<stbir_pixel_layout>(3));
 
+    resized_image.width = new_width;
+    resized_image.height = new_height;
+    resized_image.channels = 3;
+    resized_image.rgb_image = resized_image_data;
+
+    return resized_image;
 }
+
